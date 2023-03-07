299a8989bc697760d2211f799d1bfe346fd6c648
Krzysiek Przystarz <frodson35@gmail.com>
2023-03-07 20:00:55 +0100

    zadanie 16.1

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,24 @@
 package pl.javastart.task;
 
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[] {"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[] {"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[] {"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[] {"grudzień", "styczeń", "luty"});
+
+    private final String plName;
+    private final String[] months;
+
+    Season(String plName, String[] months) {
+        this.plName = plName;
+        this.months = months;
+    }
+
+    public String getPlName() {
+        return plName;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
 }
